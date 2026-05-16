f88ea35761d387ffe905c1e1277cbe85bcaecd79
Grzegorz Żytliński <mikiapr2006@gmail.com>
2026-05-16 13:55:40 +0200

    PIerwszy commit projektu

--- main.cpp
+++ main.cpp
@@ -1,21 +1,8 @@
-#include "SFML/Graphics.hpp" // w tym miejscu nawiasy ostre (youtube uniemożliwia umieszczenie ich w opisie filmu)
-
-int main(void) {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
+#include "SFML/Graphics.hpp" 
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout<< "gc";
     return 0;
 }
